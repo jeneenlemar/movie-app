d20d22ecf794c4a264ff523a829b87dd0aa130b8
Jeneen <jeneen.lemar@gmail.com>
2020-01-21 09:19:13 -0500

    adds RESTful routes, controller actions and views for Movie model

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,16 +1,66 @@
 class Api::MoviesController < ApplicationController
 
-  def all_movies_method
+
+
+  def index
     @movies = Movie.all
-    render "all_movies.json.jb"
+    render "index.json.jb"
   end
 
+  def show
+    @movie = Movie.find_by(id: params[:id]) 
+    render "show.json.jb"
+  end
 
-  def first_movie_method
-    @movie = Movie.first
-    render "first_movie.json.jb"
+  def create 
+    @movie = Movie.new(
+      title: params[:title],
+      year: params[:year],
+      plot: params[:plot]
+      )
+    @movie.save
+    render "show.json.jb"
+  end
+
+  def update
     
+    @movie = movie.find_by(id: params[:id])
+   
+
+  
+    @movie.title = params[title] || @movie.title
+    @movie.year = params[:year] || @movie.year
+    @movie.plot = params[:plot] || @movie.plot
+
+    @movie.save
+
+    render "show.json.jb"  
   end
 
+  def destroy
 
+    @movie = Movie.find_by(id: params[:id])
+    @movie.destroy
+    render json: {message: "Record has been successfully deleted"}
+  end
+
+    
+  
+
+
+  
 end
+
+  # def all_movies_method
+  #   @movies = Movie.all
+  #   render "all_movies.json.jb"
+  # end
+
+
+  # def first_movie_method
+  #   @movie = Movie.first
+  #   render "first_movie.json.jb"
+    
+  # end
+
+
