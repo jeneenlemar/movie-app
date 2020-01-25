3bd15e9a9919b0503e92f4ed271c2a814ccab859
Jeneen <jeneen.lemar@gmail.com>
2020-01-24 22:29:22 -0500

    fixes default data value to true for movies model english attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_23_023727) do
+ActiveRecord::Schema.define(version: 2020_01_25_032555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_01_23_023727) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "director"
-    t.boolean "english"
+    t.boolean "english", default: true
   end
 
 end
