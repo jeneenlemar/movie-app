b9a43c32f3dfcd2c58a42dbebc199aba030b1bd8
Jeneen <jeneen.lemar@gmail.com>
2020-02-15 10:07:44 -0500

    adds js webrequest and axion

---
+++ public/index.js
@@ -0,0 +1,8 @@
+/* global axios */
+
+console.log("connection test");
+
+axios.get("http://localhost:3000/api/actors").then(function(response) {
+  var actors = response.data;
+  console.log(actors);
+});
